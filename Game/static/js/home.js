4618fbe26f33db04cbe48717d6d784c1927f1752
babarkhandev <b.khan01@hotmail.co.uk>
2024-02-02 18:49:12 +0000

    Create documentation Create admin page Add icon Add waiting logic Add Colour Palette

--- Game/static/js/home.js
+++ Game/static/js/home.js
@@ -5,6 +5,10 @@ async function main() {
     console.log("Loaded JS")
 }
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 // Build the page that a user first sees when they log in
 async function build_login() {
 
@@ -43,11 +47,64 @@ async function check_username() {
         ),
     })
 
-    let success_or_fail = await response.json()
-    if (success_or_fail === "success") {
-        alert("Success")
+    let response_msg = await response.json()
+    if (response_msg === "initial_user") {
+        build_ready_up_button()
+    }
+    else if (response_msg === "success") {
+        await build_waiting_for_state(2, "Waiting For All Players To Be Ready")
+        build_harvesting_page()
     } else {
         alert("Username is taken, try again")
     }
-
 }
+
+async function build_waiting_for_state(state, waiting_text){
+
+    let text_element = document.createElement("p")
+    text_element.innerHTML = waiting_text
+
+    let main_section = document.getElementById("main")
+    main_section.innerHTML = ""
+    main_section.appendChild(text_element)
+
+    while (true){
+        let response = await fetch("get_state")
+        let response_msg = await response.json()
+
+        if (response_msg === state){
+            return
+        }
+
+        await sleep(1000)
+    }
+}
+
+function build_ready_up_button(){
+
+    let ready_button = document.createElement("button")
+    ready_button.innerHTML = "All Players Ready"
+    ready_button.id = "ready_button"
+    ready_button.addEventListener("click", () => submit_ready())
+
+    let main_section = document.getElementById("main")
+    main_section.innerHTML = ""
+    main_section.appendChild(ready_button)
+}
+
+async function submit_ready(){
+    let response = await fetch("/all_ready")
+    let response_msg = await response.json()
+    if (response_msg === "success"){
+        build_harvesting_page()
+    }
+    else{
+        alert("Something went wrong, try again :(")
+    }
+}
+
+function build_harvesting_page(){
+    let main_section = document.getElementById("main")
+    main_section.innerHTML = "Harvesting Time!"
+}
+
