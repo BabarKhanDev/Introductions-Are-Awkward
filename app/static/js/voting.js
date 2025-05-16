7329873bc36cb959ec5fd0e87c7976ffb6811926
BabarKhanDev <b.khan01@hotmail.co.uk>
2025-05-16 19:54:26 +0100

    add dropdowns to select the player you think it is

--- app/static/js/voting.js
+++ app/static/js/voting.js
@@ -5,6 +5,7 @@ export async function build_voting_page() {
     let main_section = document.getElementById("main")
     main_section.innerHTML = "Please drag the introduction to the matching player"
 
+    // Get all introductions
     let response = await fetch(`/${sessionStorage.getItem("game_key")}/all_introductions`, {
         method: "POST",
         cache: "no-cache",
@@ -12,66 +13,53 @@ export async function build_voting_page() {
         referrerPolicy: "no-referrer",
         body: JSON.stringify({"username": sessionStorage.getItem("username")}),
     })
-
     let introductions = await response.json()
 
+    // Set up voting area
     let voting_area = document.createElement("div")
     voting_area.id = "voting_area"
 
-    let name_area = document.createElement("div")
-    name_area.id = "name_area"
 
     let intro_area = document.createElement("div")
     intro_area.id = "intro_area"
 
-    // This is adapted from https://stackoverflow.com/a/74336249
-    intro_area.addEventListener("dragover", (e) => {
-        e.preventDefault();
-        const afterElement = getDragAfterElement(intro_area, e.clientX, e.clientY);
-        const draggable = document.querySelector(".dragging");
-        if (afterElement == null) {
-            intro_area.appendChild(draggable);
-        } else {
-            intro_area.insertBefore(draggable, afterElement);
-        }
-    });
-
-    voting_area.append(name_area, intro_area)
+    voting_area.append(intro_area)
     main_section.append(voting_area)
 
-    introductions.players.forEach((name) => {
-        let name_elem = document.createElement("p")
-        name_elem.className = "voting_name"
-        name_elem.textContent = name
-        name_area.append(name_elem)
-    })
-
+    // For each introduction, make an element for it
     introductions.introductions.forEach((intro) => {
-        let intro_elem = document.createElement("p")
+        let intro_elem = document.createElement("div")
         intro_elem.className = "voting_intro"
-        intro_elem.setAttribute("draggable", "true")
-        intro_elem.textContent = intro
-        intro_elem.addEventListener("dragstart", () => {
-            intro_elem.classList.add("dragging")
-        });
-        intro_elem.addEventListener("dragend", () => {
-            intro_elem.classList.remove("dragging")
-        });
+
+        let intro_text = document.createElement("p")
+        intro_text.className = "intro_text"
+        intro_text.textContent = intro
+
+        // Add list of all players to our introduction
+        let player_vote_selector = document.createElement("select")
+        player_vote_selector.className = "player_vote_selector"
+        introductions.players.forEach((name) => {
+            player_vote_selector.options.add( new Option(name, name))
+        })
+        intro_elem.append(intro_text, player_vote_selector)
+
         intro_area.append(intro_elem)
     })
 
+    // Set up submit button
     let submit_button = document.createElement("button")
     submit_button.id = "voting_submit"
     submit_button.innerText = "Submit"
     submit_button.addEventListener("click", async () => {
 
-        let names = document.getElementsByClassName("voting_name")
         let intros = document.getElementsByClassName("voting_intro")
 
         let guess = {}
-        for (let i = 0; i < names.length; i++) {
-            guess[names[i].textContent] = intros[i].textContent
+        for (let i = 0; i < intros.length; i++) {
+            let selected_name = intros[i].querySelector(".player_vote_selector").value
+            guess[selected_name] = intros[i].querySelector(".intro_text").textContent
         }
+        console.log(guess)
 
         response = await fetch(`/${sessionStorage.getItem("game_key")}/submit_guess`, {
             method: "POST",
@@ -91,17 +79,4 @@ export async function build_voting_page() {
         await build_results_page()
     })
     voting_area.append(submit_button)
-}
-
-function getDragAfterElement(container, x) {
-    const draggableElements = [...container.querySelectorAll('.voting_intro:not(.dragging)')]
-    return draggableElements.reduce((closest, child) => {
-        const box = child.getBoundingClientRect()
-        const offset = x - (box.left + box.width / 2)
-        if (offset < 0 && offset > closest.offset) {
-            return {offset: offset, element: child}
-        } else {
-            return closest
-        }
-    }, {offset: Number.NEGATIVE_INFINITY}).element
 }
